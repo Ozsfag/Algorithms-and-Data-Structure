16c4a99a23b47fbe5b194ad0717d63f8cb4ec090
ozsfag <ozsfag3154artem@gmail.com>
2024-07-11 15:59:50 +0900

    VeryDynamicArray task

--- 03_HashMaps/src/homework/VeryDynamic/Main.java
+++ 03_HashMaps/src/homework/VeryDynamic/Main.java
@@ -7,12 +7,18 @@ public class Main {
         VeryDynamicArray veryDynamicArray = new VeryDynamicArray();
         veryDynamicArray.add(1);
         veryDynamicArray.add(2);
-        veryDynamicArray.add(1);
-        veryDynamicArray.add(2);
-        veryDynamicArray.add(2);
-        veryDynamicArray.add(1);
+        veryDynamicArray.add(3);
+        veryDynamicArray.add(4);
+        veryDynamicArray.add(5);
+        veryDynamicArray.add(6);
+        veryDynamicArray.add(7);
+        veryDynamicArray.add(8);
+        veryDynamicArray.add(9);
 
         System.out.println(veryDynamicArray);
+        veryDynamicArray.deleteElementAt(1);
+        veryDynamicArray.deleteElementAt(1);
+        veryDynamicArray.deleteElementAt(1);
         veryDynamicArray.deleteElementAt(1);
         System.out.println(veryDynamicArray);
     }
--- 03_HashMaps/src/homework/VeryDynamic/VeryDynamicArray.java
+++ 03_HashMaps/src/homework/VeryDynamic/VeryDynamicArray.java
@@ -2,6 +2,8 @@ package homework.VeryDynamic;
 
 import examples.Dynamic.DynamicArray;
 
+import java.util.Arrays;
+
 public class VeryDynamicArray extends DynamicArray {
     int[] values = super.values;
     int size = super.size;
@@ -10,9 +12,19 @@ public class VeryDynamicArray extends DynamicArray {
         for (int i = index; i < values.length - 1; i++){
             values[i] = values[i + 1];
         }
+        values[values.length - 1] = 0;
+        decreaseResize();
     }
 
     public void decreaseResize(){
-
+        int currenSize = 0;
+        for (int val : values){
+            if (val != 0) currenSize += 1;
+        }
+        if ((super.size / 4) > currenSize){
+            super.values = Arrays.copyOf(values, size / 4);
+            super.size = super.values.length;
+            super.currentIndex = super.size;
+        }
     }
 }
